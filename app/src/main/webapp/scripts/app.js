4b46cb8fc902f30401b9818e17238cc2b1a6be3d
Martin Reinhardt <contact@martinreinhardt-online.de>
2014-09-11 10:52:49 +0200

    Improved client side error handling

--- app/src/main/webapp/scripts/app.js
+++ app/src/main/webapp/scripts/app.js
@@ -1,4 +1,5 @@
 var app = angular.module('angularCordovaApp', [
+  'fsCordova',
   'ngRoute',
   'ngTouch',
   'mobile-angular-ui',
--- app/src/main/webapp/scripts/controllers/main.js
+++ app/src/main/webapp/scripts/controllers/main.js
@@ -1,12 +1,17 @@
-app.controller('AppController', function($rootScope, $scope) {
-  $rootScope.loading = false;
+app.controller('AppController', function($rootScope, $scope, CordovaService) {
 
-  $rootScope.$on("$routeChangeStart", function() {
-    $rootScope.loading = true;
-  });
-
-  $rootScope.$on("$routeChangeSuccess", function() {
+  CordovaService.ready.then(function() {
+    // Cordova is ready
     $rootScope.loading = false;
+
+    $rootScope.$on("$routeChangeStart", function() {
+      $rootScope.loading = true;
+    });
+
+    $rootScope.$on("$routeChangeSuccess", function() {
+      $rootScope.loading = false;
+    });
+
   });
 
 });
@@ -16,32 +21,45 @@ app.controller('ChatController', function($rootScope, $scope, $routeParams, Sett
     settings = SettingsService.load(),
     sender = settings.username,
     connection;
+
+  // TODO use ngSocket https://github.com/angular/ngSocket
   var url = 'ws://' + settings.server + '/message/' + sender;
 
   function initChat() {
+    $rootScope.loading = true;
     var self = this;
     $scope.messages = [];
     $scope.sender = sender;
+    $scope.recipient = recipient;
     connection = new WebSocket(url);
 
     connection.onopen = function() {
       console.log('Connected to chat service');
+      $scope.$apply(function() {
+        $rootScope.loading = false;
+      });
     }
     connection.onclose = function() {
       console.log('Connection to chat service closed.');
     }
     connection.onerror = function(error) {
       console.log('Error in chat service' + error);
+      if (navigator.notification) {
+        navigator.notification.alert('Could not connect to chat service backend. Please choose chat partner again.', null, 'Error in chat service', 'OK');
+      }
     }
     connection.onmessage = function(event) {
+      $rootScope.loading = true;
       var msg = angular.fromJson(event.data);
       $scope.$apply(function() {
         $scope.messages.push(msg);
+        $rootScope.loading = false;
       });
     }
   }
 
   function sendMessage() {
+    $rootScope.loading = true;
     var msg = {
       to: recipient,
       from: sender,
@@ -49,6 +67,7 @@ app.controller('ChatController', function($rootScope, $scope, $routeParams, Sett
     };
     connection.send(JSON.stringify(msg));
     $scope.message = '';
+    $rootScope.loading = false;
   }
 
   // public methods
@@ -63,9 +82,11 @@ app.controller('ChatController', function($rootScope, $scope, $routeParams, Sett
 app.controller('UsersController', function($rootScope, $scope, UserService) {
 
   function loadUsers() {
+    $rootScope.loading = true;
     UserService.listAllUsers().then(
       function(response) {
         $scope.users = response.data;
+        $rootScope.loading = false;
       }
     );
   }
