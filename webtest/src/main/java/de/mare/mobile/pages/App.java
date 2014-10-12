3b08cb6e1eade2650639f58a05ac13b6cf19350f
Martin Reinhardt <contact@martinreinhardt-online.de>
2014-10-12 17:55:41 +0200

    Fix UI test errors

--- webtest/src/main/java/de/mare/mobile/pages/App.java
+++ webtest/src/main/java/de/mare/mobile/pages/App.java
@@ -1,11 +1,14 @@
 package de.mare.mobile.pages;
 
+import net.thucydides.core.annotations.findby.FindBy;
 import net.thucydides.core.pages.PageObject;
 
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
 public class App extends PageObject {
+
+	@FindBy(id = "navbar")
+	private WebElement navBar;
 
 	@FindBy(xpath = "//*[@id='home']/a")
 	private WebElement menuItemHome;
@@ -16,17 +19,26 @@ public class App extends PageObject {
 	@FindBy(xpath = "//*[@id='settings']/a")
 	private WebElement menuItemSettings;
 
+	public void openNavBarIfNeeded() {
+		if (element(navBar).isVisible()) {
+			element(menuItemHome).click();
+		}
+	}
+
 	public void clickHomeItem() {
+		openNavBarIfNeeded();
 		element(menuItemHome).waitUntilVisible();
 		element(menuItemHome).click();
 	}
 
 	public void clickUsersItem() {
+		openNavBarIfNeeded();
 		element(menuItemUsers).waitUntilVisible();
 		element(menuItemUsers).click();
 	}
 
 	public void clickSettingsItem() {
+		openNavBarIfNeeded();
 		element(menuItemSettings).waitUntilVisible();
 		element(menuItemSettings).click();
 	}
